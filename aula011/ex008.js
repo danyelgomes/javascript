ae94e1d6bef7b1e7554a1431693ee67a1580d44b
Danyel Gomes <danyeldev1@gmail.com>
2025-05-11 16:54:47 -0300

    wd

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velociade do seu carro é ${vel}KM/H`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
