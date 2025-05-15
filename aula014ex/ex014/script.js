5d4b1188c25aa93bb5d4a7319700fcb9982ad6a9
Danyel Gomes <danyeldev1@gmail.com>
2025-05-15 17:29:19 -0300

    a

--- aula014ex/ex014/script.js
+++ aula014ex/ex014/script.js
@@ -11,11 +11,18 @@ var passo = Number(document.getElementById('passo').value)
 
     res.innerHTML = ''
 
- if (passo == 0 || passo == '') {
-    passo = 1
- }
-    while (start <= end) {
-        res.innerHTML += `<p>${start}</p>`
-        start = start + passo
+    if (passo == 0 || passo == '') {
+        passo = 1
     }
-}
+
+    if (start < end) {
+        while (start <= end) {
+            res.innerHTML += `<p>${start}</p>`
+            start = start + passo
+        }
+    } else {
+        while (start >= end) {
+        res.innerHTML += `<p>${start}</p>`
+        start = start - passo
+    }
+}}
