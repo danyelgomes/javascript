8704d4d9fa382232d39d695d421dafdc0b834a8d
Danyel Gomes <danyeldev1@gmail.com>
2025-05-16 12:31:46 -0300

    a

--- aula014ex/ex014/script.js
+++ aula014ex/ex014/script.js
@@ -2,7 +2,6 @@ var btn = document.getElementsByTagName('button')[0]
 var res  = document.getElementById('res')
 
 btn.addEventListener('click', contar)
-btn.addEventListener('onkeydown', contar)
 
 function contar() {
 var start = Number(document.getElementById('istart').value)
